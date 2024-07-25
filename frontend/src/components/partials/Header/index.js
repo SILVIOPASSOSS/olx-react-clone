9afe7599cdf1523f1985b8fafb6d9fcabd5545c9
SILVIOPASSOSS <silvyopassosdossantos@hotmail.com>
2024-07-25 18:31:55 -0300

    feat: structure project

---
+++ frontend/src/components/partials/Header/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { HeaderArea } from './styled';
+
+ const Header = () => {
+  return (
+    <HeaderArea>
+      headerArea
+    </HeaderArea>
+  )
+}
+
+export default Header;
